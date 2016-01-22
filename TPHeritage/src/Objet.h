171cbec48f8e14b53107347657554ba7cf937d06
gaoyiqin95 <yiqin.gao@insa-lyon.fr>
2016-01-22 23:50:38 +0800

    Ajout de Objet.h et Point.h

---
+++ TPHeritage/src/Objet.h
@@ -0,0 +1,110 @@
+/*************************************************************************
+                           Objet  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Objet> (fichier Objet.h) ------
+#if ! defined ( OBJET_H )
+#define OBJET_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include <string>
+#include <vector>
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Objet>
+//
+//
+//------------------------------------------------------------------------
+
+class Objet
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    /**Tester si le point en parametre est dans l'objet au courant
+     *
+     *@param unPoint le point que l'on fait le test
+     *@return true si le point est dans l'objet et sinon false
+     */
+	virtual bool estDedans ( Point unPoint );
+
+	/**Preparer la chaine de la description de l'objet pour l'affichage
+	 *
+	 *@return la chaine pour l'affichage de la description de l'objet
+	 */
+    virtual std::string Description ();
+
+	/**Preparer la chaine pour reconstruire l'objet au courant
+	 *
+	 *@return la chaine pour reconstruire l'objet
+	 */
+    virtual std::string CommandeReconstruire ();
+
+    std::string getNom()
+    {
+    	return nom;
+    }
+
+//------------------------------------------------- Surcharge d'opérateurs
+    Objet & operator = ( const Objet & unObjet );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Objet ( const Objet & unObjet );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+	/**Creer un objet a partir d'un nom
+	 *
+	 *@param unNom le nom fourni pour l'objet
+	 */
+    Objet (std::string unNom ):nom(unNom){}
+
+    virtual ~Objet ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+private:
+//------------------------------------------------------- Méthodes privées
+
+protected:
+//----------------------------------------------------- Attributs protégés
+    std::string nom;
+    std::vector<Point*> listePoints;
+
+private:
+//------------------------------------------------------- Attributs privés
+
+//---------------------------------------------------------- Classes amies
+
+//-------------------------------------------------------- Classes privées
+
+//----------------------------------------------------------- Types privés
+
+};
+
+//----------------------------------------- Types dépendants de <Objet>
+
+#endif // OBJET_H
+
